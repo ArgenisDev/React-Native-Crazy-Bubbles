74d8a1fa510f38210a2135b49ff8fe3a0ebe7c72
ArgenisDev <argenisdev@gmail.com>
2024-04-05 17:38:44 -0500

    fix: issue build app

--- android/app/src/main/java/com/reactnativecrazybubbles/MainApplication.kt
+++ android/app/src/main/java/com/reactnativecrazybubbles/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.animationparticles
+package com.reactnativecrazybubbles
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/reactNativeCrazyBubblesTests/reactNativeCrazyBubblesTests.m
+++ ios/reactNativeCrazyBubblesTests/reactNativeCrazyBubblesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface animationParticlesTests : XCTestCase
+@interface reactNativeCrazyBubblesTests : XCTestCase
 
 @end
 
-@implementation animationParticlesTests
+@implementation reactNativeCrazyBubblesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
